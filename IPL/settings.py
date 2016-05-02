89bd85058e27f8b5831b23c90f8e4d2dcc26ed72
Lallu Anthoor <lalluanthoor@gmail.com>
2016-05-03 01:43:50 +0530

    theme configuration

--- IPL/settings.py
+++ IPL/settings.py
@@ -122,8 +122,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'vb/static')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'vb/static')
+    #os.path.join(BASE_DIR, 'vb/static')
 ]

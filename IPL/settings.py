87090737d0f4663aaefa11ca315941661becb3a9
Lallu Anthoor <lalluanthoor@gmail.com>
2016-05-03 01:28:56 +0530

    theme configuration

--- IPL/settings.py
+++ IPL/settings.py
@@ -120,9 +120,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/vb/static/'
+STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = 'vb'
 
 STATICFILES_DIRS = [
     #os.path.join(BASE_DIR, 'vb/static')

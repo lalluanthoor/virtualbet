085f07d99f3dcfcd53dfb385657ee4332bc67b25
Lallu Anthoor <lalluanthoor@gmail.com>
2016-05-08 15:15:59 +0530

    Change password

--- vb/views.py
+++ vb/views.py
@@ -59,7 +59,7 @@ def changepassword(request):
         if request.method == 'POST':
             return manageCentral.changePassword(request)
         else:
-            form = PasswordForm()
+            form = forms.PasswordChangeForm(user=request.user)
             theme = Configuration.objects.get(pk=1).theme.theme_name
             return HttpResponse(render(request, 'bet/changepassword.html', context={'form': form, 'theme': theme, 'title': 'Change Password'}))
     else:
